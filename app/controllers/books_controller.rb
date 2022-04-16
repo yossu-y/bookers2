ecf4b852f1c85f39cb76a65832bd1a5e8d7d90a9
yossu-y <yangyuub@gmail.com>
2022-04-16 08:08:13 +0000

    [finish]ページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,32 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def index
+    @books = Book.page(params[:book])
   end
 
   def show
   end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to ""
+  end
+  
+  private
+  
+  def books_params
+    params.require(:book).permit(:title, :option)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
+  get "homes/about" => "homes#top",as: "about"
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :books, only: [:new, :index, :show]
-  
-  resources :users, only: [:show, :edit]
-  
+  resources :books, only: [:new, :index, :show, :create, :destroy]
+
+  resources :users, only: [:show, :edit, :update]
+
 end
