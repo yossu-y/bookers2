498e2da3642eab993b78a51bc23710c3e8f07446
yossu-y <yangyuub@gmail.com>
2022-04-16 01:23:47 +0000

    [finish]checksheet13

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
   root to: 'homes#top'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
