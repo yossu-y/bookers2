37af20311655f1f5bde9d54faa527074aad988cf
yossu-y <yangyuub@gmail.com>
2022-04-19 09:33:36 +0000

    [update]レイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update, :index]
   resources :books
-  post 'books' => 'books#create'
   root to: 'homes#top'
   get "/home/about" => "homes#about", as: "about"
 
